936cb6d3ba80ec2a0df53612434213b8b8219336
Brendan Ware <brendan.ware7@t-mobile.com>
2022-11-15 15:56:08 -0800

    switch report_file to default

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ use std::path::Path;
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(name = "coverage-reporter")]
-#[command(author = "Brendan W. <bware43@gmail.com>")]
 #[command(version = "0.1.0")]
 #[command(about = "Generates .md coverage reports", long_about = None)]
 struct Args {
     /// Path to the report file to parse
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "./lcov.info")]
     report_file: String,
 }
 
